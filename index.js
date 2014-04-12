fe3c0e97b1ded3a2e8591e246fe2496e3fc320d7
braun <braun@brauns-iMac.local>
2014-04-12 17:41:46 -0400

    switched to troll

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var mongodb = require('mongodb');
 var mongoClient = mongodb.MongoClient;
 var database = null;
 
-mongoClient.connect('mongodb://localhost:20000/facebyte',mongodbClientConnect_handler);
+mongoClient.connect('mongodb://localhost:20000/troll',mongodbClientConnect_handler);
 
 function mongodbClientConnect_handler (error,_database){
 	if (error) {
@@ -23,11 +23,11 @@ http.get('/',httpGet_index);
 http.get('/users',httpGet_users);
 http.post('/users/:username/:password',httpPost_users);
 http.post('/users/tokens/:username/:password',httpPost_usersTokens);
-http.post('/users/photos/:facebyte/:token',httpPost_usersPhotos);
+http.post('/users/photos/:troll/:token',httpPost_usersPhotos);
 
 function httpGet_index (request,response) {
 	// jkhasdkjhaksjdfkhjsd
-	response.send('facebyte.io\n');
+	response.send('troll.io\n');
 }
 
 function httpGet_users (request,response) {
@@ -102,11 +102,11 @@ function httpPost_usersPhotos (request,response) {
 		var users = database.collection('users');
 		var user = {
 			token: request.params.token,
-			facebyte: request.params.facebyte
+			troll: request.params.troll
 		};
 		users.update(
 			{token:user.token},
-			{$set:{facebyte:request.params.facebyte}},
+			{$set:{troll:request.params.troll}},
 			function usersUpdate_callback (error,objects) {
 				if (error) {
 					console.warn(error.message);
