94bdbd471845dfebf8ef6adacbb6bcc0870f110a
braun <braun@brauns-iMac.local>
2014-04-12 15:30:00 -0400

    well defined data administration

--- create_db.sh
+++ create_db.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 MONGO=$1
-$MONGO --port 20000 ./create_db.js
+
+if [ ! -d "mongodb.data" ]; then
+	mkdir mongodb.data
+fi
+
+if [ -d "mongodb.data" ]; then
+	$MONGO --port 20000 ./create_db.js
+fi
